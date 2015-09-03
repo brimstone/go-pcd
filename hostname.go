08193ff1a4d04187e185b1c8ed02b3e29624c6cb
Matt <brimstone@the.narro.ws>
2015-09-03 19:07:58 -0400

    Adding in hostname ctl functions

--- hostname.go
+++ hostname.go
@@ -5,11 +5,26 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 func init() {
-	http.HandleFunc(API_VERSION+"/hostname", handleHostname)
+	http.HandleFunc("/"+API_VERSION+"/hostname", handleHostname)
+	cmds = append(cmds, &cobra.Command{
+		Use:   "hostname [hostname]",
+		Short: "Get or Set Hostname",
+		Long:  "This gets or sets the hostname for the system.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				fmt.Printf("%s", APIGet("hostname"))
+			} else if len(args) == 1 {
+				APIPost("hostname", args[0])
+			} else {
+				cmd.Help()
+			}
+		},
+	})
 	inits = append(inits, func() {
 		hostname := viper.GetString("hostname")
 		if hostname != "" {
