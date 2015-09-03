87bc25135a9d05b0e39bc5699edc222a476188c1
Matt <brimstone@the.narro.ws>
2015-09-03 18:58:15 -0400

    Adding general api functions

---
+++ api.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func APIGet(path string) string {
+	resp, err := http.Get(BASE_URL + "/" + API_VERSION + "/" + path)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return string(body)
+}
+
+func APIPost(path string, payload string) {
+	_, err := http.Post(BASE_URL+"/"+API_VERSION+"/"+path,
+		"text/plain",
+		bytes.NewBufferString(payload))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
