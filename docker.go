04f4549b0875ea820bcaadc70a715c0daa0bb587
Matt <brimstone@the.narro.ws>
2015-09-03 18:58:45 -0400

    Adding docker cli commands

--- docker.go
+++ docker.go
@@ -7,16 +7,31 @@ import (
 	"os"
 	"time"
 
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var restartTimer *time.Timer
 
 func init() {
-	http.HandleFunc(API_VERSION+"/docker/bip", handleDockerBip)
+	http.HandleFunc("/"+API_VERSION+"/docker/bip", handleDockerBip)
 	inits = append(inits, func() {
 		WriteDockerConfig()
 		RestartDocker()
+	})
+	cmds = append(cmds, &cobra.Command{
+		Use:   "docker/bip [address]",
+		Short: "Get or Set Docker Bridge IP",
+		Long:  "This gets or sets the IP for the internal docker bridge.",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				fmt.Println(APIGet("docker/bip"))
+			} else if len(args) == 1 {
+				APIPost("docker/bip", args[0])
+			} else {
+				cmd.Help()
+			}
+		},
 	})
 }
 
